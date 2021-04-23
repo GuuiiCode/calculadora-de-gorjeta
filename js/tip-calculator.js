8c62d7ae418009140ec3124d80bb8517f1a38678
Guilherme Alves <mixgui2010@yahoo.com.br>
2021-04-22 23:33:18 -0300

    segregação do css/js em pastas

---
+++ js/tip-calculator.js
@@ -0,0 +1,31 @@
+
+let container = document.getElementById('container');
+
+container.addEventListener('input', update);
+
+function update() {
+    let bill = Number(document.getElementById("yourBill").value);
+    let tipPercent = document.getElementById("tipInput").value;
+    let split = document.getElementById("splitInput").value;
+
+    let tipValue = bill * (tipPercent / 100); 
+    let tipEach = tipValue / split;  
+    let newBillEach = (bill + tipValue) / split;
+
+    document.getElementById("tipPercent").innerHTML = tipPercent + "%";
+    document.getElementById("tipValue").innerHTML = formatMoney(tipValue); 
+    document.getElementById("totalWithTip").innerHTML = formatMoney(bill + tipValue);
+    document.getElementById("splitValue").innerHTML = formatSplit(split);
+    document.getElementById("billEach").innerHTML = formatMoney(newBillEach);    
+    document.getElementById("tipEach").innerHTML = formatMoney(tipEach);  
+}
+
+function formatMoney(value) {
+    value = value.toFixed(2);
+    return "$ " + value;
+}
+
+function formatSplit(value) {
+    if(value === "1") return value + " person";
+    return value + " people";
+}
